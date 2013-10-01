4424a02db304f60dd8dc78a5b6c92e5b86e131f3
Marek Kubica <marek@xivilization.net>
2013-10-02 00:10:45 +0200

    Added some minimal Swing frame

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject lifevis "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Game of life visualization"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [seesaw "1.4.3"]]
   :main lifevis.core
-  :profiles {:uberjar {:aot :all}})
+;  :profiles {:uberjar {:aot :all}})
+  )
--- src/lifevis/core.clj
+++ src/lifevis/core.clj
@@ -1,7 +1,15 @@
 (ns lifevis.core
+  (:use seesaw.core)
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  (invoke-later
+    (-> (frame :title "Foo"
+               :content (grid-panel :rows 2 :items [(label :background :white) "Two" "Three" (label :background :black)])
+               :on-close :exit
+               :minimum-size [300 :by 300])
+        pack!
+        show!))
   (println "Hello, World!"))
