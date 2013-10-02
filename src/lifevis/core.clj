cc7f8baee029ea4a148e682a2e2be3e1d177b692
Marek Kubica <marek@xivilization.net>
2013-10-03 00:35:16 +0200

    Made the simulation do something interactively

--- src/lifevis/core.clj
+++ src/lifevis/core.clj
@@ -1,15 +1,38 @@
 (ns lifevis.core
   (:use seesaw.core)
+  (:require [seesaw.bind :as b]
+            [seesaw.timer :as t])
   (:gen-class))
 
+(def starting-data [[true false] [false true]])
+
+(defn build-widgets [data]
+  (->> data
+       (apply concat)
+       (map #(if % :black :white))
+       (map #(label :background %))))
+
+(defn random-seq []
+  (repeatedly #(rand-nth [true false])))
+
+(defn dummy-simulate [data]
+  (println data)
+  (map vector (take 4 (random-seq))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Displays a SWING window with game of life cells"
   [& args]
   (invoke-later
-    (-> (frame :title "Foo"
-               :content (grid-panel :rows 2 :items [(label :background :white) "Two" "Three" (label :background :black)])
-               :on-close :exit
-               :minimum-size [300 :by 300])
-        pack!
-        show!))
-  (println "Hello, World!"))
+    (let [grid (grid-panel :rows 2)
+          data (ref starting-data)]
+      (-> (frame :title "Game of Life"
+                 :content grid
+                 :on-close :exit
+                 :minimum-size [300 :by 300])
+          pack!
+          show!)
+      (config! grid :items (build-widgets @data))
+      (t/timer (fn [state]
+                 (dosync
+                   (alter data dummy-simulate))
+                 (config! grid :items (build-widgets @data)))))))
